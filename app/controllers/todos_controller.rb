b67dcc97a7fd4a112d023377e7c57b334fc5c1ac
Zsolt Szalma <zsolt.szalma@gmail.com>
2024-06-06 22:44:23 +0200

    Implement CRUD for todos

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,53 @@
+class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+    @uncompleted_todos_count = uncompleted_count
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todos_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todos_params)
+      redirect_to todos_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_url, status: :see_other
+  end
+
+  private
+
+  def todos_params
+    params
+      .require(:todo)
+      .permit(:title)
+  end
+
+  def uncompleted_count
+    @todos.where(completed: false).count
+  end
+
+end
