4570c0a2844a18007eb4bcb60620e0a650a1ccdd
Zsolt Szalma <zsolt.szalma@gmail.com>
2024-06-08 21:54:52 +0200

    Generate User model with secure password

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,7 @@
 class Todo < ApplicationRecord
 
+  belongs_to :user
+
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_190557) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_195221) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
