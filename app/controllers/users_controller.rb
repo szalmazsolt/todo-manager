3626335ef1e194009946e3453abd94fa362a497a
Zsolt Szalma <zsolt.szalma@gmail.com>
2024-06-11 18:16:54 +0200

    Finish REST actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,28 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "You have successsfully updated your profile!"
+      redirect_to user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "You have successsfully deleted your account!"
+    redirect_to root_url, status: :see_other
+  end
+
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   has_secure_password
   has_many :todos, dependent: :destroy
 
-  validates :username, length: { minimum: 3, maximum: 25}, uniqueness: true
+
+  validates :username, length: { minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false }
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 
-  validates :password, length: { minimum: 6, maximum: 25}
+  validates :password, length: { minimum: 6, maximum: 25}, allow_nil: true
 
 end
